59b4b8d7a468c541b8bdc5585ef5149068983cac
Fernando Arconada <fernando.arconada@gmail.com>
2010-11-21 11:45:50 +0100

    Se añaden iconos y wizzard para CE wizzard tipo plugin list

---
+++ Classes/Util/class.tx_f2contentce_wizicon.php
@@ -0,0 +1,47 @@
+<?php
+class tx_f2contentce_wizicon {
+
+	/**
+	 * Adds the newloginbox wizard icon
+	 *
+	 * @param	array		Input array with wizard items for plugins
+	 * @return	array		Modified input array, having the item for newloginbox added.
+	 */
+	function proc($wizardItems)	{
+		$wizardItems['plugins_tx_f2contentce_feed'] = $this->getWizzardArray('feed');
+		$wizardItems['plugins_tx_f2contentce_gmpas'] = $this->getWizzardArray('gmaps');
+		$wizardItems['plugins_tx_f2contentce_video'] = $this->getWizzardArray('video');
+		$wizardItems['plugins_tx_f2contentce_gallery'] = $this->getWizzardArray('gallery');
+
+		return $wizardItems;
+	}
+
+	/**
+	 * Includes the locallang file for the 'tt_news' extension
+	 *
+	 * @return	array		The LOCAL_LANG array
+	 */
+	function includeLocalLang()	{
+		$llFile = t3lib_extMgm::extPath('f2contentce').'Resources/Private/Language/locallang.xml';
+		$LOCAL_LANG = t3lib_div::readLLXMLfile($llFile, $GLOBALS['LANG']->lang);
+		return $LOCAL_LANG;
+	}
+
+	private function getWizzardArray($plugin){
+		global $LANG;
+		$LL = $this->includeLocalLang();
+
+		$wizardItems = array(
+			'icon'=>t3lib_extMgm::extRelPath('f2contentce').'Resources/Public/Icons/'.$plugin.'.gif',
+			'title'=>$LANG->getLLL($plugin.'_wiz_title',$LL),
+			'description'=>$LANG->getLLL($plugin.'_wiz_description',$LL),
+			'params'=>'&defVals[tt_content][CType]=list&defVals[tt_content][list_type]=f2contentce_'.$plugin
+		);
+
+		return $wizardItems;
+	}
+
+}
+
+
+?>
